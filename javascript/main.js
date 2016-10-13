4e06a2b8488b921ea3cab6bed8884190acb96d17
Justin Co <jcoen5209@gmail.com>
2016-10-13 18:40:09 -0400

    Refactoring displayResults

--- javascript/main.js
+++ javascript/main.js
@@ -1,104 +1,88 @@
-//Tasks
-
-	// insure bare bones funcationality without javascript
-
-	// 'unobtrusive javascript'
-
-	// display 10 students at a time
-
-	// depending on total number of students, dynamically generate appropiate amount of page links
-
-	// on page click, display next appropiate listings
-
-	// use 'progressive enhancement' to add the search HTML
-		// this should mean simply inject the HTML with javascript
-		// since those without javascript cannot use it, they wont see it either
-
-	// implement search funcationality to filter users
-		// perhaps HTML element filtering
-		// such as an h3's innerHTML/text storage
-
-	// searchable by both name and email
-		// also partial input search/filter
-
-	// 'pagination' functionality with search results if results exceed 10
-
-	// * animations on page load
-
-	// * as user types, dynamically display results 
-		// perhaps an on keypress event
-
-	// * if no matches are found produce an HTML error message
-
 //To Do Next:
 
-// //store all students
-
-// //generate buttons based on calculation
+// change button to anchor w/ parent div & ul/li's w/ appropiate classes & active marker
 
 
 
-// display 10 students at a time
-	// .children() creates an array
-	// 
+/* 
+	hide all students except for the first 10 -
+	generate search HTML & append to the .page-header -
+
+*/
+//$(".student-item:nth-of-type(n+11)").hide();
+$(".page-header").append(
+	'<div class="student-search">' +
+		'<input placeholder="Search for Students...">' +
+		'<button>Search</button>' +
+	'</div>'
+);
+$(".page").append('<div id="student-pages"></div>');
 
 
 
+/* 
+	pageAmount calculates needed pages based on number of students / 10 -
+	'for loop' generates page HTML based on pageAmount value -
+*/
+var pageAmount,
+	tenStudents;
 
+function paginationAmount(x) {
 
+	$("#student-pages").html("");
 
+	pageAmount = Math.ceil(x / 10);
 
+	for(var i = 1; i <= pageAmount; i++) {
+		$("#student-pages").append("<button>" + i + "</button>");
+	}
 
-
-
-
-
-
-
-
-
-$(".student-item").hide();
-$(".page").append('<div id="studentPages"></div>');
-
-var tenStudents;
-var studentLength = $(".student-item").length;
-var pageAmount = Math.ceil(studentLength / 10);
-
-// generate buttons based on student amount Math.ceil
-// unobtrusivley 
-for(var i = 1; i <= pageAmount; i++) {
-	$("#studentPages").append("<button>" + i + "</button>");
 }
 
-// on page click hide current listings
-// store the index number of button that was clicked
-// using tenStudents value with 'i' iteration, show corrosponding listings
-/* 
-	this must adapt with search functionality -
-	currently the function takes in a single array - 
-	the for loop must take in an argument - 
-	should be possible with modification and a default fallback -
-	or another 'undefined' possibility -
-	also undefineds must be handled correctly -
-*/ 
-$("#studentPages button").click(function() {
+function displayResults(x) {
+	for(var i = 0; i < 10; i++) {
+		x[10+i].show();
+	}
+}
+
+function searchStudent() {
 
 	$(".student-item").hide();
 
-	tenStudents = $("#studentPages button").index(this);
+	var searchVal = new RegExp($(".student-search input").val());
+	var resultsArr = [];
 
-	for(var i = 0; i < 10; i++) {
-		$(".student-item:eq(" + tenStudents + i + ")").show();
+	for(var i = 0; i < $(".student-item").length; i++) {
+
+		if(searchVal.test($(".student-item:eq(" + i + ") h3").text()) == true) {
+			resultsArr.push($(".student-item:eq(" + i + ")"));
+		}
+
 	}
 
-})
+	paginationAmount(resultsArr.length);
+	displayResults(resultsArr);
+}
 
 
 
+searchStudent();
+
+$(".student-search input").keypress(function() {
+	searchStudent();
+});
+
+
+$("#student-pages button").click(function() {
+
+
+});
 
 
 
-
+$(".student-search button").click(function() {
+	searchStudent();
+});
 
 
 
