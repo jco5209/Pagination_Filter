be6f3d885b606b2a5354ce1e915684acf378b4d1
Justin Co <jcoen5209@gmail.com>
2016-10-17 11:45:22 -0400

    Polished and commented for project submission

--- javascript/main.js
+++ javascript/main.js
@@ -1,87 +1,122 @@
-//To Do Next:
-
-// change button to anchor w/ parent div & ul/li's w/ appropiate classes & active marker
-
-
+/* global $ */
 
 /* 
-	hide all students except for the first 10 -
-	generate search HTML & append to the .page-header -
-
+	generate search & pagination HTML interface 
 */
-//$(".student-item:nth-of-type(n+11)").hide();
 $(".page-header").append(
 	'<div class="student-search">' +
 		'<input placeholder="Search for Students...">' +
 		'<button>Search</button>' +
 	'</div>'
 );
-$(".page").append('<div id="student-pages"></div>');
-
+$(".page").append('<div class="pagination"><ul></ul></div>');
 
 
 /* 
 	pageAmount calculates needed pages based on number of students / 10 -
-	'for loop' generates page HTML based on pageAmount value -
+	'for loop' generates page HTML based on pageAmount value 
 */
-var pageAmount,
-	tenStudents;
-
 function paginationAmount(x) {
 
-	$("#student-pages").html("");
+	$(".pagination ul").html("");
 
-	pageAmount = Math.ceil(x / 10);
+	 var pageAmount = Math.ceil(x / 10);
 
 	for(var i = 1; i <= pageAmount; i++) {
-		$("#student-pages").append("<button>" + i + "</button>");
-	}
 
+		if(i == 1) {
+			$(".pagination ul").append("<li><a class='active' href='#'>" + i + "</a></li>");
+		}
+		else {
+			$(".pagination ul").append("<li><a href='#'>" + i + "</a></li>");
+		}
+	}
 }
 
-function displayResults(x) {
+
+/*
+	currentPage stores the indice value of the currently selected page -
+	concat currentPage value and current 'i' iteration to select appropiate students -
+	loop breaks if no more students are available
+*/
+function displayResults() {
+
+	var currentPage = $(".pagination a").index($(".active"));
+
 	for(var i = 0; i < 10; i++) {
-		x[10+i].show();
+
+		if(searchResults[parseInt("" + currentPage + i)] === undefined) {
+			break;
+		}
+		else {
+			searchResults[parseInt("" + currentPage + i)].show();
+		}
 	}
 }
+
+
+/* 
+	stores student search field input as a new Regular Expression -
+	empty previous searchResults array -
+	loops through all students to search for any matching results based on the inputted value -
+	if a match is true for either student h3 field or email field, push into searchResults array -
+	length of searchResults is passed to paginationAmount() to calculate # of pages to generate -
+	if no results are found display 'No Matches Found...' message
+*/
+var searchResults = [];	
 
 function searchStudent() {
 
 	$(".student-item").hide();
 
 	var searchVal = new RegExp($(".student-search input").val());
-	var resultsArr = [];
+	
+	searchResults = [];
 
 	for(var i = 0; i < $(".student-item").length; i++) {
 
-		if(searchVal.test($(".student-item:eq(" + i + ") h3").text()) == true) {
-			resultsArr.push($(".student-item:eq(" + i + ")"));
+		if(searchVal.test($(".student-details:eq(" + i + ") h3").text()) === true || searchVal.test($(".student-details:eq(" + i + ") span").text()) === true) {
+			searchResults.push($(".student-item:eq(" + i + ")"));
 		}
 
 	}
 
-	paginationAmount(resultsArr.length);
-	displayResults(resultsArr);
+	paginationAmount(searchResults.length);
+
+	if(searchResults.length === 0) {
+		$(".page .no-results").remove();
+		$(".page").append("<p class='no-results'>No Matches Found...</p>");
+	} else {
+		$(".page .no-results").remove();
+		displayResults();
+	}
 }
 
 
-
+// search for & display all students on page load
 searchStudent();
 
-$(".student-search input").keypress(function() {
+$(".student-search input").keydown(function() {
 	searchStudent();
 });
-
-
-$("#student-pages button").click(function() {
-
-
-});
-
-
 
 $(".student-search button").click(function() {
 	searchStudent();
+});
+
+
+/*
+	on pagination anchor click, remove previous anchor class that has class of '.active' -
+	addClass of '.active' to anchor that was clicked -
+	hide previously displayed students -
+	add a loading animation while displayResults is queued to fire
+*/
+$(".pagination").on("click", "a", function() {
+	$(".pagination a").removeClass('active');
+	$(this).addClass('active');
+	$(".student-item").hide();
+	$(".pagination").prepend("<div class='load-block0'></div> <div class='load-block1'></div> <div class='load-block2'></div>");
+	setTimeout(function() { displayResults(); $(".pagination div").remove(); }, 750);
 });
 
 
